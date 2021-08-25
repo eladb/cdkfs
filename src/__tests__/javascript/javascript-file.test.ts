7916ab4cc8199954c7367620baaaa40eebb60dfb
Mark McCulloh <Mark.McCulloh@gmail.com>
2021-08-25 03:20:08 +0000

    wip

---
+++ src/__tests__/javascript/javascript-file.test.ts
@@ -0,0 +1,74 @@
+import { FileBase } from '../../file';
+import { JavascriptFile } from '../../javascript/javascript-file';
+import { LogLevel } from '../../logger';
+import { NodeProject, NodeProjectOptions } from '../../node-project';
+import { mkdtemp, synthSnapshot } from '../util';
+
+test('can add javascript file component', () => {
+  const prj = new TestNodeProject({
+    name: 'test-project',
+    defaultReleaseBranch: 'main',
+  });
+
+  new JavascriptFile(prj, 'cool.config.js', {
+    fn: () => {
+      console.log('Hello World');
+    },
+  });
+
+  const out = synthSnapshot(prj);
+  const outFile = out['cool.config.js'];
+  console.log(outFile);
+
+  expect(outFile).toBe(`// ${FileBase.PROJEN_MARKER}\nconsole.log('Hello World');\n`);
+  expect(outFile).toMatchSnapshot();
+});
+
+test('can add javascript file component with various options', () => {
+  const prj = new TestNodeProject({
+    name: 'test-project',
+    defaultReleaseBranch: 'main',
+  });
+
+  const file = new JavascriptFile(prj, 'cool.config.js', {
+    fn: (name) => {
+      return `Hello ${name}`;
+    },
+    keepDeclaration: true,
+    callWith: [prj.name],
+    setToValue: 'greeting',
+  });
+
+  file.addFunction({
+    fn: (greeting) => {
+      console.log(greeting);
+    },
+    prefix: '// do a log\n',
+    postfix: '\n// did a log',
+  });
+
+  const out = synthSnapshot(prj);
+  const outFile = out['cool.config.js'];
+
+  console.log(outFile);
+
+  expect(outFile).toContain('const greeting = (name =>');
+  expect(outFile).toContain('(`test-project`)');
+  expect(outFile).toContain('// do a log');
+  expect(outFile).toContain('// did a log');
+  expect(outFile).toMatchSnapshot();
+});
+
+class TestNodeProject extends NodeProject {
+  constructor(options: Partial<NodeProjectOptions> = {}) {
+    super({
+      outdir: mkdtemp(),
+      name: 'test-node-project',
+      logging: {
+        level: LogLevel.OFF,
+      },
+      defaultReleaseBranch: 'master',
+      ...options,
+    });
+  }
+}
---
+++ src/javascript/javascript-file.ts
@@ -0,0 +1,78 @@
+import { js } from 'js-beautify';
+import { TextFileOptions, TextFile, Project, FileBase } from '..';
+
+export function getFunctionBody(fn: IJavascriptFunction) {
+  let fString = fn.fn.toString().trim();
+  if (!fn.keepDeclaration) {
+    if (fn.callWith) {
+      throw new Error('callWith and keepDeclaration must both be true');
+    }
+    fString = fString.substring(fString.indexOf('{') + 1, fString.lastIndexOf('}'));
+  } else {
+    if (fn.callWith) {
+      fString = `(${fString})(${fn.callWith.map(c => `\`${c.toString()}\``).join(', ') ?? ''})`;
+    }
+  }
+  if (fn.setToValue) {
+    fString = `const ${fn.setToValue} = ${fString};`;
+  }
+  if (fn.prefix) {
+    fString = fn.prefix + fString;
+  }
+  if (fn.postfix) {
+    fString = fString + fn.postfix;
+  }
+  return fString;
+};
+
+export interface IJavascriptFunction {
+  setToValue?: string;
+  keepDeclaration?: boolean;
+  prefix?: string;
+  postfix?: string;
+  callWith?: any[];
+  fn(..._: any): any;
+}
+
+export interface IJavascriptFileOptions extends IJavascriptFunction {
+  textFileOptions?: TextFileOptions;
+}
+
+export class JavascriptFile extends TextFile {
+  private functions: IJavascriptFunction[];
+
+  constructor(project: Project, path: string, options?: IJavascriptFileOptions) {
+    super(project, path, options?.textFileOptions);
+
+    this.functions = [];
+
+    if (options?.fn) {
+      this.addFunction(options);
+    }
+  }
+
+  addFunction(fn: IJavascriptFunction): void {
+    this.functions.push(fn);
+  }
+
+  prependFunction(fn: IJavascriptFunction): void {
+    this.functions.unshift(fn);
+  }
+
+  preSynthesize() {
+    if (this.readonly) {
+      this.addLine(`// ${FileBase.PROJEN_MARKER}`);
+    }
+
+    const code = this.functions.map(f => getFunctionBody(f)).join('\n');
+
+    this.addLine(js(code, {
+      indent_level: 0,
+      indent_empty_lines: false,
+      indent_size: 2,
+      end_with_newline: true,
+      preserve_newlines: true,
+      max_preserve_newlines: 2,
+    }));
+  }
+}
